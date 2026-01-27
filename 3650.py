95ffb9bc86828dfd8e092e6a9b85020647fd0787
NNON71 <110216423+NNON71@users.noreply.github.com>
2026-01-27 20:39:38 +0700

    add 1/27/2026

---
+++ 3650.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def minCost(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        g = [[] for _ in range(n)]
+        for x, y, w in edges:
+            g[x].append((y, w))
+            g[y].append((x, 2 * w))
+
+        dist = [float("inf")] * n
+        visited = [False] * n
+        dist[0] = 0
+        heap = [(0, 0)]  # (Distance, Node)
+
+        while heap:
+            cur_dist, x = heapq.heappop(heap)
+
+            if x == n - 1:
+                return cur_dist
+
+            # already processed
+            if visited[x]:
+                continue
+            visited[x] = True
+
+            # relaxing neighbors
+            for y, w in g[x]:
+                new_dist = cur_dist + w
+                if new_dist < dist[y]:
+                    dist[y] = new_dist
+                    heapq.heappush(heap, (new_dist, y))
+
+        return -1
