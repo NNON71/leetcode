51927e3578e1bdfb8498ed167f5c563f9c77fee0
NNON71 <110216423+NNON71@users.noreply.github.com>
2026-02-04 19:36:04 +0700

    git commit "fix 2/4/2026"

--- 3640.py
+++ 3640.py
@@ -4,43 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        dp0 = [float('-inf')]*n
-        dp1 = [float('-inf')]*n
-        dp2 = [float('-inf')]*n
-
-        for i in range(1, n):
-            # increasing
-            if nums[i - 1] < nums[i]:
-                new = float('-inf')
-                val = nums[i - 1] + nums[i]
-                if dp0[i - 1] != 0:
-                    new = dp0[i - 1] + nums[i]
-                dp0[i] = max(val, new)
-
-            # decreasing
-            if nums[i - 1] > nums[i]:
-                best_prev_sum = float('-inf')
-                if dp1[i - 1] != 0:
-                    best_prev_sum = dp1[i - 1]
-                if dp0[i - 1] != 0:
-                    best_prev_sum = max(best_prev_sum, dp0[i - 1])
-                if best_prev_sum != 0:
-                    dp1[i] = best_prev_sum + nums[i]
-            
-            # incresing
-            if nums[i - 1] < nums[i]:
-                best_prev_sum = float('-inf')
-                if dp2[i - 1] != 0:
-                    best_prev_sum = dp2[i - 1]
-                if dp1[i - 1] != 0:
-                    best_prev_sum = max(best_prev_sum, dp1[i - 1])
-                if best_prev_sum != 0:
-                    dp2[i] = best_prev_sum + nums[i]
-        print(dp0)
-        print(dp1)
-        print(dp2)
-        ans = float('-inf')
-        for x in dp2:
-            ans = max(ans, x)
-        return ans
+        INF = -(1 << 50)
+        result = a = b = c = INF
+        prev = nums[0]
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            na = nb = nc = INF
+            if curr > prev:
+                na = max(a, prev) + curr
+                nc = max(b, c) + curr
+            elif curr < prev:
+                nb = max(a, b) + curr
+            a, b, c = na, nb, nc
+            result = max(result, c)
+            prev = curr
+        return result
